471e9201ff9260547ba782f1e4a75996777c49ec
PAM <sasserjacob@gmail.com>
2017-06-12 16:15:01 +0000

    Update views and navigation

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :user_stocks
-  devise_for :users
+  devise_for :users, :controllers => { :registrations => "user/registrations" }
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
